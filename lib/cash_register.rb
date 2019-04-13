6a6c76f26a4e4651c2877fdda5f30c4da8624296
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-13 02:35:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -31,7 +31,7 @@ class CashRegister
     @items
   end
   
-  def void_last_transaction (price, quantity=1)
+  def void_last_transaction (price, quantity)
     @last_trans = price*quantity
     @total -= @last_trans
   end
