7bcb31f2c8841049d6f43c30c8639da1e1f6b68a
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-13 02:38:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister 
   
-  attr_accessor :total, :last_trans
-  attr_reader :discount
+  attr_accessor :total, :last_trans, :discount
+  
   
   def initialize(discount=0)
     @total = 0 
